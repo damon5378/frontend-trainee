19232dc2c5b5404eab58d8f4522a7b03a0cd4ebb
beginner-frontender <44082329+damon5378@users.noreply.github.com>
2019-10-29 12:59:12 +0300

    Add files via upload

--- activebox/src/js/script.js
+++ activebox/src/js/script.js
@@ -12,4 +12,132 @@ $(window).unbind('scroll');
 }
 });
 
+    /* FIXED HEADER */
+
+let header = $("#header");
+let intro = $("#intro");
+let introH = intro.innerHeight();
+let scrollPos = $(window).scrollTop();
+
+let nav = $("#nav");
+let navToggle = $("#navToggle");
+
+$(window).on("scroll load resize", function() {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+    if(scrollPos > introH) {
+        header.addClass("fixed");
+    } else {
+        header.removeClass("fixed");
+    }
+});
+
+
+
+header.addClass("fixed");
+
+
+/* SMOOTH MENU */
+
+$("[data-scroll]").on("click", function(e){
+    e.preventDefault();
+
+    let elementID = $(this).data('scroll');
+    let elementOffset = $(elementID).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate({
+        scrollTop: elementOffset - 60
+    }, 1300);
+});
+
+
+/* BURGER */
+
+navToggle.on("click", function(e){
+    e.preventDefault();
+
+    nav.toggleClass("show")
+});
+
+/* REVIEWS */
+
+let slider = $("#reviewsSlider");
+
+slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+    autoplay: true
+  });
+
+
+  /* MODAL */
+
+  let modalCall = $("[data-modal]");
+  let modalClose = $("[data-close]");
+
+  modalCall.on("click", function(e){
+    e.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $(modalId).addClass('show');
+    $("body").addClass('no-scroll');
+
+    setTimeout(function(){
+        $(modalId).find('.modal__dialog').css({
+            transform: "rotateX(0)"
+        });
+    }, 200);
+
+    
+  });
+
+  modalClose.on("click", function(e){
+    e.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents('.modal');
+
+    modalParent.find('.modal__dialog').css({
+            transform: "rotateX(90deg)"
+        });
+
+        setTimeout(function(){
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+        });
+        
+    
+
+    
+
+  $(".modal").on("click", function(e){
+      let $this = $(this);
+      $this.find('.modal__dialog').css({
+        transform: "rotateX(90deg)"
+    });
+
+    setTimeout(function(){
+        $this.removeClass('show');
+        $("body").removeClass('no-scroll');
+    }, 200);
+    
+    
+  });
+
+  $(".modal__dialog").on("click", function(e){
+    e.stopPropagation();
+  });
+
+  
+
 });
---
+++ activebox/src/telegram.php
@@ -0,0 +1,30 @@
+<?php
+
+/* https://api.telegram.org/bot936412545:AAFskHFj3w9pc9HKpQ5GBJsfOMHJdic7apc/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$message = $_POST['user_message'];
+$token = "936412545:AAFskHFj3w9pc9HKpQ5GBJsfOMHJdic7apc";
+$chat_id = "-378185761";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email: ' => $email,
+  'Сообщение: ' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
